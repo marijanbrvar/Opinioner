e2a177fb1cce096004a1ad3ec4544ffe00a2ce4e
marijanbrvar <marijan.brvar@gmail.com>
2021-04-22 22:31:31 +0200

    Add User model

---
+++ db/migrate/20210422203107_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :full_name
+      t.text :photo
+      t.text :cover_image
+
+      t.timestamps
+    end
+    add_index :users, :username, unique: true
+  end
+end
