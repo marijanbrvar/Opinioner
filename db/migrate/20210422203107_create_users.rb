8a8fb5bd0b5cdbbcc3260a9016d08be42ef21700
marijanbrvar <marijan.brvar@gmail.com>
2021-04-22 22:56:49 +0200

    Add user validation and default value

--- db/migrate/20210422203107_create_users.rb
+++ db/migrate/20210422203107_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :full_name
+      t.string :username, null: false
+      t.string :full_name, null: false
       t.text :photo
       t.text :cover_image
 
