b47b1eec52747c06366785ed30a988bc61857aa9
marijanbrvar <marijan.brvar@gmail.com>
2021-04-23 01:56:33 +0200

    Add styles to login and register views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_path, notice: 'Logged out!'
+    redirect_to login_path, notice: 'You are logged out now.'
   end
 end
