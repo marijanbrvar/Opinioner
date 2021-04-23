e1176f654d436f32f1c991fd37987abe485d9bb6
marijanbrvar <marijan.brvar@gmail.com>
2021-04-23 02:58:13 +0200

    Add styles to app layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.photo ||= Placeholder.image_generator(height: '150', width: '150')
+    self.photo ||= Placeholder.image_generator(height: '75', width: '75')
     self.cover_image ||= Placeholder.image_generator(height: '900', width: '250')
   end
 end
