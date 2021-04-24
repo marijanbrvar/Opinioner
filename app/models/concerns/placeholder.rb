b699bd306bd61f8876086926769594582ca19384
marijanbrvar <marijan.brvar@gmail.com>
2021-04-24 10:45:47 +0200

    Add  placeholder for new user profile default values

--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -2,6 +2,10 @@ module Placeholder
   extend ActiveSupport::Concern
 
   def self.image_generator(height:, width:)
-    "https://via.placeholder.com/#{width}x#{height}"
+    "http://placeimg.com/#{width}/#{height}/any"
+  end
+  
+  def self.avatar_generator(size:, imgno:)
+    "https://pravatar.cc/#{size}?img=#{imgno}"
   end
 end
