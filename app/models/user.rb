e28ce23e6146b0b99fd65386e6344fa6cd27f843
marijanbrvar <marijan.brvar@gmail.com>
2021-04-24 11:50:11 +0200

    Add elements with style on main

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
-  validates :full_name, presence: true
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Following'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Following'
+  has_many :following, through: :following_relationships, source: :following
+
+  has_many :opinions, foreign_key: 'author_id'
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+  validates :full_name, presence: true, length: { minimum: 5, maximum: 20 }
 
   after_initialize :set_defaults
 
