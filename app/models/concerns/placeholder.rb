ca10145a89cd8622f3bc15daa563b876397c5021
marijanbrvar <marijan.brvar@gmail.com>
2021-04-26 02:04:13 +0200

    Fix bacground image issue

--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -2,7 +2,7 @@ module Placeholder
   extend ActiveSupport::Concern
 
   def self.image_generator(height:, width:)
-    "http://placeimg.com/#{width}/#{height}/any"
+    "https://placeimg.com/#{width}/#{height}/any"
   end
 
   def self.avatar_generator(size:, imgno:)
