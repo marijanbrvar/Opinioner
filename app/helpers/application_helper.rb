7d5a94ec14f538251f12b83485624c9d3a7af061
marijanbrvar <marijan.brvar@gmail.com>
2021-04-29 11:40:33 +0200

    Fix remove logic from views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def system_messages
+    if flash[:notice]
+      render partial: 'shared/notice'
+    elsif flash[:alert]
+      render partial: 'shared/alert'
+    end
+  end
+  
+  def form_messages(value)
+    if value.errors.any?
+      render partial: 'shared/forms_alert', locals: {value: value }
+    end
+  end
 end
